1856955916d57d19c73a329348661e7c7c8a0684
annkonieczna <aniakonieczna16@gmail.com>
2025-09-26 18:29:34 +0200

    usunięcie plików

--- SEGZAMIN/2024/kspojny2024.py
+++
@@ -1,8 +0,0 @@
-#Mamy ciąg liczb reprezentowany w tablicy  𝑇 [0], …, 𝑇 [𝑛 − 1].
-# Ciąg jest k- spójny jeśli można go stworzyć poprzez wybranie pewnego zakresu elementów  𝑇 [𝑖], 𝑇 [𝑖 + 1], 𝑇 [𝑖 + 2], …, 𝑇 [𝑗]
-# i usunięciu co najwyżej k elementów 
-
-
-
-
-
